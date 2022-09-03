dc0447c841b4412f9062fb795c95764c951b13c3
Dell <Dell@IT>
2022-09-03 20:59:57 +0530

    Space Test

--- Selinium/src/automation/opneBrowser.java
+++ Selinium/src/automation/opneBrowser.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class opneBrowser {
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "D:\\IT\\Cyber\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
